0e5d949294d012fed70c321e1a8a74025fae5280
Alesya Maximenko <a-lesya.net@mail.ru>
2024-01-26 16:12:58 +0300

    add models

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hi");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT, DB_URL } = process.env;
+const app = express();
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log({ message: 'i work just fine' });
+});
